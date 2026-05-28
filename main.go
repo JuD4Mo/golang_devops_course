a55c7e84d539977a28cf301de3c369ba92126422
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-05-28 03:15:58 -0500

    Refactor main.go to implement Response interface and improve error handling

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
 )
+
+type Response interface {
+	GetResponse() string
+}
 
 type Page struct {
 	Name string `json:"page"`
@@ -20,8 +23,21 @@ type Words struct {
 	Words []string `json:"words"`
 }
 
+func (w Words) GetResponse() string {
+	return fmt.Sprintf("%s", strings.Join(w.Words, ", "))
+}
+
 type Occurrence struct {
 	Words map[string]int `json:"words"`
+}
+
+func (o Occurrence) GetResponse() string {
+	out := []string{}
+
+	for word, occurrence := range o.Words {
+		out = append(out, fmt.Sprintf("%s (%d)", word, occurrence))
+	}
+	return fmt.Sprintf("%s", strings.Join(out, ", "))
 }
 
 func main() {
@@ -32,15 +48,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	if _, err := url.ParseRequestURI(args[1]); err != nil {
-		fmt.Printf("Usage: ./http-get <url>\n\nURL is not valid URL: %s\n", args[1])
+	res, err := doRequest(args[1])
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	response, err := http.Get(args[1])
+	if res == nil {
+		fmt.Printf("no response\n")
+		os.Exit(1)
+	}
+
+	fmt.Printf("response: %s\n", res.GetResponse())
+}
+
+func doRequest(requestURL string) (Response, error) {
+	if _, err := url.ParseRequestURI(requestURL); err != nil {
+		return nil, fmt.Errorf("Usage: ./http-get <url>\n\nURL is not valid URL: %s\n", requestURL)
+	}
+
+	response, err := http.Get(requestURL)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("ReadAll error: %s", err)
 	}
 
 	defer response.Body.Close()
@@ -48,19 +78,18 @@ func main() {
 	body, err := io.ReadAll(response.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("unmarshal error: %s", err)
 	}
 
 	if response.StatusCode != 200 {
-		fmt.Printf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, string(body))
-		os.Exit(1)
+		return nil, fmt.Errorf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, string(body))
 	}
 
 	var page Page
 
 	err = json.Unmarshal(body, &page)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("unmarshal error: %s", err)
 	}
 
 	switch page.Name {
@@ -69,28 +98,19 @@ func main() {
 
 		err = json.Unmarshal(body, &words)
 		if err != nil {
-			log.Fatal(err)
+			return nil, fmt.Errorf("unmarshal error: %s", err)
 		}
-		fmt.Printf("JSON: Parsed:\nPage: %s\nWords: %s\n", page.Name, strings.Join(words.Words, ", "))
+		return words, nil
 
 	case "occurrence":
 		var occurence Occurrence
 
 		err = json.Unmarshal(body, &occurence)
 		if err != nil {
-			log.Fatal(err)
+			return nil, fmt.Errorf("unmarshal error: %s", err)
 		}
 
-		if val, ok := occurence.Words["word1"]; ok {
-			fmt.Printf("Found word1: %d\n", val)
-		}
-
-		for word, occurence := range occurence.Words {
-			fmt.Printf("%s: %d\n", word, occurence)
-		}
-
-	default:
-		fmt.Printf("page not found\n")
+		return occurence, nil
 	}
-
+	return nil, nil
 }
